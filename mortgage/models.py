df12f1731457dc511cf4736657c7c135b8d1cec0
Brook Rodia <brookrodia@gmail.com>
2020-02-25 21:03:17 -0500

    Added Mortgage App to settings.py and updated Models.py

--- mortgage/models.py
+++ mortgage/models.py
@@ -1,17 +1,19 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-#User profile model
+# User profile model
+
+
 class Profile(models.Model):
-    user = models.ForeignKey(User)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     first_name = models.CharField(max_length=255)
-    last_name = models.IntegerField()
+    last_name = models.CharField(max_length=255)
     email = models.CharField(max_length=255)
 
-#Vendor model
+# Vendor model
+
+
 class Vendor(models.Model):
     name = models.CharField(max_length=255)
     location = models.CharField(max_length=255)
     balance = models.IntegerField()
-
-
