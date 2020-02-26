47ccf1b958b29610ea9351e1e02fe770de1c27a4
Fred Astaire <lancebc@thegodardgroup.com>
2020-02-25 20:42:26 -0500

    fixed urls.py

--- mohawk/urls.py
+++ mohawk/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from .views import index, CreateView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index.as_view()),
