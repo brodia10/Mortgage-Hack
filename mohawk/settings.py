ef7b48f31fc4db3726ece95ce9474ad491014076
Brook Rodia <brookrodia@gmail.com>
2020-02-27 21:34:48 -0500

    turned off debug

--- mohawk/settings.py
+++ mohawk/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'uxnn=bro9wm90s=utpuj9vxm)l^dorfr&mj20)^+bysu7_u$k@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['mo-hawk.herokuapp.com']
 
